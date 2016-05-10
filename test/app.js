42a27710dbc0ef3593ba01133c9c14d56c79e365
Jacob Sachs <jacob.s.sachs@gmail.com>
2016-05-10 16:20:33 -0400

    fix root GET to account for no auth redirect

--- test/app.js
+++ test/app.js
@@ -2,10 +2,10 @@ var request = require('supertest');
 var app = require('../app.js');
 
 describe('GET /', function() {
-  it('should return 200 OK', function(done) {
+  it('should return 302 if not logged in', function(done) {
     request(app)
       .get('/')
-      .expect(200, done);
+      .expect(302, done);
   });
 });
 
