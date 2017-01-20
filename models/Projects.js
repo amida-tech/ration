eda850664929089acbf7fa1b7b79a158776321a7
Jacob Sachs <jsachs@users.noreply.github.com>
2017-01-20 18:14:28 -0500

    Projects view
    * Conditional Display of tables
    
    Prevents hard errors when you click on ‘My Hours’ in Native State.
    
    * change input type.
    
    * stub view.
    
    * Basic projects api and better pug.
    
    * project save.
    
    * embed project lookup in my hours.
    
    * Select looks at project list.
    
    * Move from header bar to menu.
    
    * remove logging statement.
    
    * fix deprecation, expose Projects view
    
    * grab project name to save correctly
    
    * prevent duplicate projects, retrieve names correctly
    
    * I messed up a filter

---
+++ models/Projects.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+
+var projectSchema = new mongoose.Schema({
+    projectId: Number,
+    name: String
+});
+
+/**
+ * Multikey index
+ */
+projectSchema.index({
+    projectId: 1,
+    name: 1
+}, {
+    unique: true
+});
+
+var Projects = mongoose.model('Projects', projectSchema);
+
+module.exports = Projects;
