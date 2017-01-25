b4a10d16d8bcaa4602a005a9b3d5d01e1a87c1e2
Matthew McCall <matt@mattmccall.com>
2017-01-25 11:28:13 -0800

    Nest testing before/afters.

--- test/models/user.spec.js
+++ test/models/user.spec.js
@@ -4,57 +4,69 @@ var User = require('../../models/User');
 
 describe('User Model', function () {
 
-    before(function (done) {
-        User.remove({}, done);
-    });
+    describe('Basic', function () {
 
-    after(function (done) {
-        User.remove({}, done);
-    });
+        before(function (done) {
+            User.remove({}, done);
+        });
 
-    it('should create a new user', function (done) {
-        var user = new User({
-            email: 'test@gmail.com',
-            password: 'password'
+        after(function (done) {
+            User.remove({}, done);
         });
-        user.save(function (err) {
-            if (err) return done(err);
-            done();
-        });
-    });
 
-    it('should not create a user with the unique email', function (done) {
-        var user = new User({
-            email: 'test@gmail.com',
-            password: 'password'
+        it('should create a new user', function (done) {
+            var user = new User({
+                email: 'test@gmail.com',
+                password: 'password'
+            });
+            user.save(function (err) {
+                if (err) return done(err);
+                done();
+            });
         });
-        user.save(function (err) {
-            if (err) err.code.should.equal(11000);
-            done();
-        });
-    });
 
-    it('should find user by email', function (done) {
-        User.findOne({
-            email: 'test@gmail.com'
-        }, function (err, user) {
-            if (err) return done(err);
-            user.email.should.equal('test@gmail.com');
-            done();
+        it('should not create a user with the unique email', function (done) {
+            var user = new User({
+                email: 'test@gmail.com',
+                password: 'password'
+            });
+            user.save(function (err) {
+                if (err) err.code.should.equal(11000);
+                done();
+            });
         });
-    });
 
-    it('should delete a user', function (done) {
-        User.remove({
-            email: 'test@gmail.com'
-        }, function (err) {
-            if (err) return done(err);
-            done();
-
+        it('should find user by email', function (done) {
+            User.findOne({
+                email: 'test@gmail.com'
+            }, function (err, user) {
+                if (err) return done(err);
+                user.email.should.equal('test@gmail.com');
+                done();
+            });
         });
+
+        it('should delete a user', function (done) {
+            User.remove({
+                email: 'test@gmail.com'
+            }, function (err) {
+                if (err) return done(err);
+                done();
+
+            });
+        });
+
     });
 
     describe('Admin', function () {
+
+        before(function (done) {
+            User.remove({}, done);
+        });
+
+        after(function (done) {
+            User.remove({}, done);
+        });
 
         it('should save an admin user', function (done) {
             var user = new User({
