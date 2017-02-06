c9e393695a7d6217c10cf4224a409b6cc5c174c5
Matthew McCall <matt@mattmccall.com>
2017-02-06 15:53:42 -0800

    pass errs in test cases.

--- test/controllers/user.spec.js
+++ test/controllers/user.spec.js
@@ -2,8 +2,6 @@ const chai = require('chai');
 const should = chai.should();
 const app = require('../../app.js');
 const api = require('supertest').agent(app);
-
-//Needed to manually set admin flag, and back out test user.
 const User = require('../../models/User');
 
 const tmpAdminUser = {
@@ -17,7 +15,7 @@ const tmpUser = {
     confirmPassword: 'asdf'
 };
 
-//Back out users, and establish root admin user.
+//Manually back out users and establish root admin user.
 before(function (done) {
     User.remove({
         email: tmpUser.email
@@ -43,24 +41,20 @@ describe.only('Admin Testing', function () {
 
     it('should not allow access if not logged in', function (done) {
         api.post('/api/account/roles').end(function (err, res) {
-            done();
+            done(err);
         })
     });
 
     it('should register user', function (done) {
         //Register endpoint returns a 302 until refactored.
         api.post('/signup').send(tmpUser).end(function (err, res) {
-            if (err) {
-                done(err);
-            } else {
-                done();
-            }
+            done(err);
         });
     });
 
     it('login admin user', function (done) {
         api.post('/login').send(tmpAdminUser).end(function (err, res) {
-            done();
+            done(err);
         });
     });
 
@@ -72,30 +66,26 @@ describe.only('Admin Testing', function () {
         };
 
         api.post('/api/account/roles').send(tmpRoleUser).expect(200).end(function (err, res) {
-            if (err) {
-                done(err);
-            } else {
-                done();
-            }
+            done(err);
         });
     });
 
     it('log out of admin user', function (done) {
         api.post('/logout').end(function (err, res) {
-            done();
+            done(err);
         });
     });
 
     it('log in as basic user', function (done) {
         api.post('/login').send(tmpUser).end(function (err, res) {
-            done();
+            done(err);
         });
     });
 
     it('check account information for admin status', function (done) {
         api.get('/api/account').end(function (err, res) {
             (res.body.roles).should.contain('admin');
-            done();
+            done(err);
         });
     });
 
