15326484c375d59ae84661289a4d7c5620f3c0e1
Matthew McCall <matt@mattmccall.com>
2017-03-07 09:28:34 -0800

    Re-enable all test cases.

--- test/controllers/user.spec.js
+++ test/controllers/user.spec.js
@@ -103,7 +103,7 @@ describe('Deactivation testing', function () {
 
 });
 
-describe.only('Delete testing', function () {
+describe('Delete testing', function () {
 
     //Restore delete user when done.
     after(function (done) {
