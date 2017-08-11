4ede81ecfd928c3e78e8e122b79e29a61b8c30fa
Jacob Sachs <jacob.s.sachs@gmail.com>
2017-08-11 00:40:18 -0400

    sort names in the hours dashboard

--- api/controllers/hours.js
+++ api/controllers/hours.js
@@ -50,6 +50,9 @@ exports.dashboard = function (req, res, next) {
             });
         }, function(err) {
             if (err) return next(err);
+            hours.sort(function(a,b) {
+                return (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0);
+            });
             res.render('hours/dashboard', {
                 title: 'Dashboard',
                 hours: hours
